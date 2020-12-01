ed52d368ca76b68dc3a9acec9da3ce120406824e
= <=>
2020-12-01 19:42:04 +0800

    更新了代码

--- ADT.py
+++ ADT.py
@@ -1,5 +1,22 @@
-class Bitree:
-    def __init__ (self, rt:char, rchild: Bitree, lchild: Bitree):
-        pass
+class Node:
+    def __init__(self, data=None, lchild=None, rchild=None):
+        self.data = data
+        self.lchild = lchild
+        self.rchild = rchild
 
 
+def huffmanTree(a: list):
+    # 认为每一个字符都是节点,如果不是,那就让它是
+    if type(a[0]) != Node:
+        a = [Node(data=i) for i in a]
+
+    b = Node()
+    a.sort(key=lambda x: x.data, reverse=True)
+    while len(a) > 1:
+        b.lchild = a.pop()
+        b.rchild = a.pop()
+        b.data = b.lchild.data + b.rchild.data
+        a.append(b)
+        a.sort(key=lambda x: x.data, reverse=True)
+
+    return a
